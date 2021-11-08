6ebb26d7d07e178f2849fe123c6106bfd6516309
Mark Furland <mtfurlan@i3detroit.org>
2021-11-08 18:05:04 -0500

    basic map kinda works

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <span v-if="loading">Loading...</span>
+        <l-map ref="myMap" style="height: 500px" :zoom="zoom" :center="center">
+            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+            <l-geo-json :geojson="geojson_outline" name="service area" :options-style="outlineStyle"/>
+            <l-geo-json :geojson="geojson_outage" name="outages" :options="options"/>
+        </l-map>
+    </div>
 </template>
 
 <script>
+import {LMap, LTileLayer, LGeoJson } from 'vue2-leaflet';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    components: {
+        LMap,
+        LTileLayer,
+        LGeoJson,
+    },
+    data () {
+        return {
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            attribution:
+            '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            zoom: 12,
+            center: [42.453606, -83.113742],
+            overlayImage: "/current.svg",
+            overlayBounds: [[41.4402817, -87.1074728],[44.4353735, -79.6697286]],
+            geojson_outline: null,
+            geojson_outage: null,
+            outlineStyle: {fillOpacity: 0},
+            loading: true,
+        };
+    },
+    async created() {
+        this.loading = true;
+        const outageResponse = fetch("/current.geojson")
+        const outlineResponse = fetch("/outline.geojson")
+        this.geojson_outline = await outlineResponse.then(r => r.json());
+        this.geojson_outage = await outageResponse.then(r => r.json());
+        this.loading = false;
+    },
+    computed: {
+        options() {
+            return {
+                onEachFeature: this.onEachFeatureFunction
+            };
+        },
+        onEachFeatureFunction() {
+            return (feature, layer) => {
+                layer.bindTooltip(
+                    Object.entries(feature.properties).reduce((out, [key, val]) => {
+                        return out + `<div>${key}: ${val}</div>`;
+                    }, ""),
+                    { permanent: false, sticky: true }
+                );
+            };
+        },
+    },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.leaflet-image-layer {
+         image-rendering: crisp-edges
 }
 </style>
